e79a271131006dfaec57e6ecebcb22dc1757a202
Coder-Akanksha <shivi.aka1810@gmail.com>
2026-08-21 23:26:24 +0530

    Add check if array is sorted and rotated solution

---
+++ src/Array_Striver/LC_Array_is_Sorted.java
@@ -0,0 +1,43 @@
+package Array_Striver;
+
+import java.util.Scanner;
+
+public class LC_Array_is_Sorted {
+
+    public static boolean check(int[] nums) {
+        int count = 0;
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > nums[(i + 1) % n]) {
+                count++;
+            }
+        }
+
+        return count <= 1;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        System.out.println(check(nums));
+
+        sc.close();
+    }
+}
+
+/*
+TC --> O(N)
+SC --> O(1)
+*/
